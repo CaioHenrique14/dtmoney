b02176bd2244ea921afffc28799a0aa829f33bc2
Caio Henrique <caiohen.ms@gmail.com>
2021-07-15 22:00:57 -0300

    #0 - Aula 2 - Modulo 2

--- src/styles/global.ts
+++ src/styles/global.ts
@@ -6,8 +6,9 @@ export const GlobalStyle = createGlobalStyle`
         --background:#f0f2f5;
         --red:#e52e4d;
         --blue:#5429cc;
+        --green:#33CC95;
 
-        --blue-title:#6933FF;
+        --blue-light:#6933FF;
 
         --text-title:#363F5F;
         --text-body:#969CB3;
